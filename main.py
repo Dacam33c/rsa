8163f727ae16733438f631b340ded139eec392c4
gabriel m <gabrielchagas3002@gmail.com>
2025-01-31 17:32:24 -0300

    erro verificacao

--- main.py
+++ main.py
@@ -75,19 +75,19 @@ def makeKey(primo1,primo2):
 
 # publicKey(e),publicExp(n),privateKey(d) = makeKey(GeradorPrimos(),GeradorPrimos())
 
-#função de encriptação. recebe um texto com hash OAEP, chave publica "e" e expoente publico "n"
-def encriptar(mensagem, e, n):
+#função de encriptação. recebe um texto com hash OAEP, chave privada "d" e expoente publico "n"
+def encriptar(mensagem, d, n):
     #transforma a mensagem codificada em um tipo que podemos operar
     msg = int.from_bytes(mensagem, 'big')
     #msg elevado a "e" no modulo "n"
-    msgEncriptada = pow(msg,e,n)
+    msgEncriptada = pow(msg,d,n)
     #retorna a mensagem com rsa e OAEP aplicados
     return(msgEncriptada)
 
-#função de decriptaçao. recebe texto encriputado e com OAEP, chave privada "d" e expoente publico "n"
-def decriptar(textoEncriptado, d, n):
+#função de decriptaçao. recebe texto encriputado e com OAEP, chave publica "e" e expoente publico "n"
+def decriptar(textoEncriptado, e, n):
     #texto elevado a "d" no modulo "n"
-    msg = pow(textoEncriptado, d, n)
+    msg = pow(textoEncriptado, e, n)
     #tamanho da mensagem
     tamanho = (n.bit_length() + 7) // 8
     #converte para bytes
@@ -96,7 +96,7 @@ def decriptar(textoEncriptado, d, n):
     return msgDecriptadaOAEP
     
 
-def mask(seed, length, hash_func=hashlib.sha256):
+def mask(seed, length, hash_func=hashlib.sha3_256):
     counter = 0
     output = b''
     while len(output) < length:
@@ -105,7 +105,7 @@ def mask(seed, length, hash_func=hashlib.sha256):
         counter += 1
     return output[:length]
 
-def encode(message, k, hash_func=hashlib.sha256):
+def encode(message, k, hash_func=hashlib.sha3_256):
     hashLen = hash_func().digest_size  # 32 bytes
     messageLen = len(message)
     label = b''
@@ -133,7 +133,7 @@ def encode(message, k, hash_func=hashlib.sha256):
 
     return b'\x00' + maskedSeed + maskeddb
 
-def decode(message,k, hash_func=hashlib.sha256):
+def decode(message,k, hash_func=hashlib.sha3_256):
     label = b''
     hashLen = hash_func().digest_size
     if len(message) != k:
@@ -146,13 +146,20 @@ def decode(message,k, hash_func=hashlib.sha256):
     seedMask = mask(maskeddb, hashLen, hash_func)
     seed = bytes(a ^ b for a, b in zip(maskedSeed, seedMask))
 
-
     #recuperando data block
     dbMask = mask(seed, len(maskeddb), hash_func)
     db = bytes(a ^ b for a, b in zip(maskeddb, dbMask))
 
     #extraindo mensagem
     tamanhoHash = hash_func(label).digest()
+
+    print(f"\ntamanhoHash = {tamanhoHash}")
+    print(f"len(tamanhoHash) = {len(tamanhoHash)}")
+    print(f"\ndb[:hashLen] = {db[:hashLen]}")
+    print(f"type(db[:hashLen]) = {len(db[:hashLen])}")
+    print(f'\n\niguais? {db[:hashLen] == tamanhoHash}')
+    
+
     if db[:hashLen] != tamanhoHash:
         raise ValueError("label hash incorreto")
 
@@ -170,7 +177,8 @@ def assinar_arquivo( file_name_ext:str, prk_file:str ) -> None:
         função pra assinar um arquivo, gerando um arquivo em base 64 da mensagem (bytes do arquivo),
         hash da mensagem e os outras informações para verificação.
         o arquivo resultante terá a extensão '.b64' e possuirá a seguinte estrutrura:
-            - primeiros n (tamanho fixo) bytes -> hash criptografado do arquivo da mensagem
+            - primeiros 2 bytes -> quantidade 'n' de caracteres do hash criptografado
+            - proximos n bytes -> hash criptografado do arquivo da mensagem
             - byte seguinte -> numero 'c' de caracteres de extensão do arquivo da mensagem
             - próximos c bytes - extensão do arquivo da mensagem
             - resto dos bytes - arquivo da mensagem
@@ -179,38 +187,55 @@ def assinar_arquivo( file_name_ext:str, prk_file:str ) -> None:
     # tenta ler arquivo da mensagem como bytes e arquivo da chave privada, retorna None em caso de erro
     try:
         with open(f'./.files/{file_name_ext}', 'rb') as f:
-            msg_bytes = f.read()
+            file_bytes = f.read()
         with open(f'./.prk/{prk_file}', 'r') as f:
-            prk = f.read()
+            prk = f.readlines()
     except Exception as exc:
         print(f'\nERRO!!!: {exc}')
         print(f"TRACEBACK:\n{traceback.format_exc()}")
         return None
 
+    d = int(prk[0], 16)
+    n = int(prk[1], 16)
+
     # caso não dê erro na leitura dos bytes do arquivo:
-    # calcula hash sha3-512 dos bytes do arquivo como um string
-    hash_str = hashlib.sha3_512(msg_bytes).hexdigest()
+    # calcula hash sha3_256 dos bytes do arquivo como um string
+    hash_str = hashlib.sha3_256(file_bytes).hexdigest()
+    
+    print(f'\nhash_str = {hash_str}')
 
-    # codifica string do hash calculado para bytes em utf-8
-    hash_bytes = hash_str.encode(encoding = "utf-8")
+    # codificação OAEP e encriptação (assinatura com a chave privada)
+    hash_bytes_encod = encode(hash_str, (d.bit_length() + 7) // 8)
+    hash_bytes_cript = str(encriptar(hash_bytes_encod, d, n)).encode("utf-8")
 
-    # hash_bytes = encriptar_rsa(hash_bytes, prk, ...)
+    print(f'\nhash_bytes_cript = {hash_bytes_cript}')
+
+    # obtem representação em bytes do tamnho do hash encriptado
+    len_hash_bytes_cript = len(hash_bytes_cript).to_bytes(2, "big")
+
+    print(f'\nlen_hash_bytes_cript = {len_hash_bytes_cript}')
+
+    # concatena comprimento do hash e o hash
+    msg_1 = len_hash_bytes_cript + hash_bytes_cript
 
     # obtém nome e extensão do arquivo
     file_name, file_extension = os.path.splitext(file_name_ext)
     num_char_file_ext = len(file_extension) - 1 # ignora o '.'
 
-    # constroi sequencia de bytes do arquivo final (assinado)
-    msg_signed = hash_bytes + num_char_file_ext.to_bytes(1, "big") + file_extension[1:].encode(encoding = "utf-8") + msg_bytes
+    # constroi dados enviados junto ao comrimento do hash e o proprio hash
+    msg_2 = num_char_file_ext.to_bytes(1, "big") + file_extension[1:].encode(encoding = "utf-8") + file_bytes
 
-    # converte para base64
-    msg_signed_b64 = base64.b64encode(msg_signed)
+    # mensagem final
+    msg_final = msg_1 + msg_2
+
+    # converte para base 64
+    msg_final_b64 = base64.b64encode(msg_final)
 
     # tenta salvar
     try:
-        with open(f".b64/{file_name}_signed.b64", "wb") as f:
-            f.write(msg_signed_b64)
-        print(f"\narquivo assinado e convertido para base 64 gerado e salvo em '.b64/{file_name}_signed.b64'\n")
+        with open(f"./.b64/{file_name}_{prk_file[:-4]}_signed.b64", "wb") as f:
+            f.write(msg_final_b64)
+        print(f"\narquivo assinado e convertido para base 64 gerado e salvo em '.b64/{file_name}_{prk_file[:-4]}_signed.b64'\n")
     except Exception as exc:
         print(f"\nERRO!!!: {exc}")
         print(f"NÃO FOI POSSÍVEL SALVAR ARQUIVO '.b64/{file_name}_signed.b64'")
@@ -229,27 +254,58 @@ def verificar_assinatura( b64_file_name:str, puk_file:str ) -> None:
         with open(f"./.b64/{b64_file_name}", "r") as f:
             b64_file = f.read()
         with open(f"./.puk/{puk_file}") as f:
-            puk = f.read()
+            puk = f.readlines()
     except Exception as exc:
         print(f"\nERRO!!!: {exc}")
         print(f"NÃO FOI POSSÍVEL LER ARQUIVOS './.b64/{b64_file_name}' e './.puk/{puk_file}'")
         print(f"TRACEBACK:\n{traceback.format_exc()}")
         return
     
-    len_hash = 128  # quantidade fixa de caracteres do hash (deve ser do criptografado, 128 é do hash em claro)
-    signed_msg_bytes = base64.b64decode(b64_file)   # decodificação base 64
-    hash_received = signed_msg_bytes[:len_hash].decode(encoding="utf-8")    # hash "recebido" no arquivo de assinatura b64
-    # hash_received = decriptar(hash_received, puk, ...)
-    num_char_file_ext = signed_msg_bytes[len_hash]
-    file_extension = signed_msg_bytes[len_hash+1:len_hash+1+num_char_file_ext].decode(encoding="utf-8")
-    msg_received_bytes = signed_msg_bytes[len_hash+1+num_char_file_ext:] # bytes da mensagem original
-    file_name = b64_file_name[:-11] # ignora o '_signed.b64'
+    e = int(puk[0], 16)
+    n = int(puk[1], 16)
+
+    print(f'\ne = {e}')
+    print(f'\nn = {n}')
+
+    # decodifica arquivo base 64 'recebido'
+    msg_bytes = base64.b64decode(b64_file)
+
+    # extrai o tamanho do hash criptografado
+    len_hash_crip = int.from_bytes(msg_bytes[:2], "big")
+
+    # extrai hash criptografado
+    hash_received_encrp = msg_bytes[2:2+len_hash_crip]
+    print(f'\nhash_received_encrp = {hash_received_encrp}')
+
+    # decriptografa com a chave pública (e,n)
+    hash_received_decrp = decriptar(int(hash_received_encrp), e, n)
+    print(f'\nhash_received_decrp = {hash_received_decrp}')
+
+    # decodifica OAEP (e depois utf-8)
+    hash_received = decode(hash_received_decrp, (e.bit_length() + 7) // 8).decode("utf-8")
+
+    # extrai numero de caracteres da extensão do arquivo
+    num_char_file_ext = int.from_bytes(msg_bytes[2+len_hash_crip:1+2+len_hash_crip],"big")
+
+    # extrai extensão do arquico
+    file_extension = msg_bytes[1+2+len_hash_crip:1+2+len_hash_crip+num_char_file_ext].decode(encoding="utf-8")
+
+    # extrai bytes do arquivo enviado
+    file_received_bytes = msg_bytes[1+2+len_hash_crip+num_char_file_ext:]
+
+    # nome do arquivo original (ignora o '_signed.b64')
+    file_name = b64_file_name[:-11]
 
     # calcula hash da mensagem recebida
-    hash_calculated = hashlib.sha3_512(msg_received_bytes).hexdigest()
+    hash_calculated = hashlib.sha3_256(file_received_bytes).hexdigest()
 
+    # flag
     validated = True
 
+    print(f'\nhash_calculated = {hash_calculated}')
+    print(f'\nhash_received = {hash_received}')
+
+    # compara os hashes (verifica assinatura)
     if hash_calculated == hash_received:
         print("\nassinatura válida !!! aeee :) ")
     else:
@@ -260,14 +316,14 @@ def verificar_assinatura( b64_file_name:str, puk_file:str ) -> None:
 
     try:
         with open(f"./.checked_files/{file_name}_{puk_file[:-4]}_{'validated' if validated else 'invalidated'}.{file_extension}", "wb") as f:
-            f.write(msg_received_bytes)
+            f.write(file_received_bytes)
     except Exception as exc:
             print(f"\nERRO!!!: {exc}")
             print("NÃO FOI POSSÍVEL RESTAURAR ARQUIVO VERIFICADO")
             print(f"TRACEBACK:\n{traceback.format_exc()}")
             return
     
-    print(f"\narquivo verificado está restaurado em checked_files/{file_name}.{file_extension}")
+    print(f"./.checked_files/{file_name}_{puk_file[:-4]}_{'validated' if validated else 'invalidated'}.{file_extension}")
 
 
 '''
@@ -281,32 +337,35 @@ def opcoes_iniciais() -> Optional[int] :
     print("\nDigite 's' para sair.")
     op = input("\n> ")
     if not op.isalnum() or not op in ("1","2","3","s","S"):
-        print("Opção inválida\n\n")
-        return 
+        print("\nOpção inválida\n\n")
     return op
 
 def salvar_chaves() -> None:
     '''
         função para salvar a chave publica na pasta .pub e a chave privada na pasta .prv
+        a primeira linha do arquivo é a expoente. a segunda linha é o n
     '''
     while True:
-        print('\nEscolha um nome para o arquivos das chaves pública e privada.')
-        print('Obs: O nome deve conter apenas caracteres alfabéticos e ter no mínimo 2 e no máximo 255 caracteres.\n')
-        print("Digite 'v' para voltar.")
-        entrada = input('>')
+        print("\nEscolha um nome para o arquivos das chaves pública e privada.")
+        print("Obs: O nome deve conter apenas caracteres alfanuméricos e ter no mínimo 2 e no máximo 255 caracteres.\n")
+        print("Digite 'v' para voltar.\n")
+        entrada = input('> ')
 
         if entrada.lower() == 'v': return
-        if not entrada.isalpha or not 2<len(entrada)<256:
-            print('O nome deve conter apenas caracteres alfabéticos e ter no mínimo 2 e no máximo 255 caracteres.\n')
+        if not entrada.isalnum() or not 2<len(entrada)<256:
+            print("\nO nome deve conter apenas caracteres alfanuméricos e ter no mínimo 2 e no máximo 255 caracteres.")
         else:
             name = entrada
-            print('Gerando chaves...')
+            print("\nGerando chaves...")
             puk, n, prk = makeKey(GeradorPrimos(), GeradorPrimos())
-
             try:
-                with open(f'./puk/{name}.puk', 'w') as f:
-                    f.write(hex(puk)[2:])
-                    f.write(hex(n)[2:])
+                with open(f"./.puk/{name}.puk", "w") as f:
+                    f.writelines([hex(puk)[2:], "\n", hex(n)[2:]])
+                    print(f"A chave publica foi salva em './.puk/{name}.puk'")
+                with open(f"./.prk/{name}.prk", "w") as f:
+                    f.writelines([hex(prk)[2:], "\n", hex(n)[2:]])
+                    print(f"A chave privada foi salva em './.prk/{name}.prk'")        
+                return
             except Exception as exc:
                 print(f"\nERRO!!!: {exc}")
                 print("NÃO FOI POSSÍVEL SALVAR CHAVES")
@@ -376,7 +435,7 @@ def opcoes_arquivos( folder:str ) -> Optional[str]:
 
 #GeradorPrimos() gera um número primo de 1024 bits
 #makeKey() recebe dois primos e retorna e,n,d
-#encode aplica codigicação OAEP
+#encode aplica codificação OAEP
 #decode remove OAEP
 #encriptar recebe mensagem com OAEP, e, n. retorna mensagem encriptada por rsa
 #decriptar recebe mensagem encriptada e com OAEP, e, d. retorna mensagem apenas com OAEP aplicado
@@ -384,9 +443,9 @@ def main():
     #exemplo do funcionamento
     #e,n,d = makeKey(GeradorPrimos(),GeradorPrimos())
     #message = "teste"    
-    #encoded_msg = encode(message, (e.bit_length() + 7) // 8)
-    #encriptada = encriptar(encoded_msg,e,n)
-    #decriptada = decriptar(encriptada,d,n)
+    #encoded_msg = encode(message, (d.bit_length() + 7) // 8)
+    #encriptada = encriptar(encoded_msg,d,n)
+    #decriptada = decriptar(encriptada,e,n)
     #decoded_msg = decode(decriptada, (e.bit_length() + 7) // 8)
     #print(decoded_msg)
 
